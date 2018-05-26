ac068d0ea15eae6aae185785348399a0aa2214cc
Kaizyn <2291443901@qq.com>
2018-05-26 11:21:20 +0800

    Nothing Important

--- test.cpp
+++ test.cpp
@@ -4,9 +4,7 @@ using namespace std;
 
 int main()
 {
-	int a , b;
-	cin >> a >> b;
-	cout << a + b << endl;
-	cout << (1<<0) << endl;
+	for(int i = 1; i <= 1e5; ++i)
+		if( (i+1)/2 != (i - i/2)) cout << i << endl;
 	return 0;
 }
