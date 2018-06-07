3e7ef7f79c80480a846ec133d319f662376984d4
Kaizyn <2291443901@qq.com>
2018-06-07 13:58:09 +0800

    OK

---
+++ luogu1803.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+const int Maxn = 1e6 + 7;
+int n, ans;
+struct Node
+{
+	int l, r;
+	bool operator < (const Node &b) const { return l == b.l ? r < b.r : l < b.l; }
+} a[Maxn];
+
+int main()
+{
+	scanf("%d", &n);
+	for(int i = 0; i < n; ++i) scanf("%d%d", &a[i].l, &a[i].r);
+	sort(a, a+n);
+	for(int i = 0, last = -1; i < n; ++i)
+		if(a[i].l >= last) { ans++; last = a[i].r; }
+		else last = min(last, a[i].r);
+	printf("%d\n", ans);
+	return 0;
+}
