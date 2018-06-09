908086ee88a90b989adfc4b2b8216a9378b87078
Kaizyn <2291443901@qq.com>
2018-06-09 17:23:06 +0800

    Nothing

--- luogu1005_2.cpp
+++ luogu1005_2.cpp
@@ -3,7 +3,6 @@
 #include <algorithm>
 #include <cstring>
 #include <vector>
-#define DEBUG
 
 using namespace std;
 
@@ -14,16 +13,17 @@ struct BigInteger
 	const static int BASE = 1e4;
 	const static int WIDTH = 4;
 
-	vector<int> v;
+	int v[100], len;
 
-	BigInteger(){}
+	BigInteger() { memset(v, 0, sizeof(int) * 100); len = 0; }
 	BigInteger(long long num) { *this = num; }
 
 	BigInteger operator = (long long num)
 	{
-		v.clear();
+		memset(v, 0, sizeof(int) * 100);
+		len = 0;
 		do{
-			v.push_back(static_cast<int>(num % BASE));
+			v[len++] = static_cast<int>(num % BASE);
 			num /= BASE;
 		} while(num);
 		return *this;
@@ -31,8 +31,8 @@ struct BigInteger
 
 	bool operator < (const BigInteger &b) const
 	{
-		if(v.size() != b.v.size()) return v.size() < b.v.size();
-		for(int i = v.size()-1; i >= 0; --i)
+		if(len != b.len) return len < b.len;
+		for(int i = len-1; i >= 0; --i)
 			if(v[i] != b.v[i])
 				return v[i] < b.v[i];
 		return false; //  ==
@@ -41,14 +41,12 @@ struct BigInteger
 	BigInteger operator + (const BigInteger &b) const
 	{
 		BigInteger res;
-		res.v.clear();
-		int g = 0;
-		for(unsigned i = 0; ; ++i)
+		for(int i = 0, g = 0; ; ++i)
 		{
-			if(g == 0 && i >= v.size() && i >= b.v.size()) break;
-			if(i < v.size()) g += v[i];
-			if(i < b.v.size()) g += b.v[i];
-			res.v.push_back(g % BASE);
+			if(g == 0 && i >= len && i >= b.len) break;
+			if(i < len) g += v[i];
+			if(i < b.len) g += b.v[i];
+			res.v[res.len++] = g % BASE;
 			g /= BASE;
 		}
 		return res;
@@ -58,17 +56,17 @@ struct BigInteger
 	BigInteger operator * (const BigInteger &b) const
 	{
 		BigInteger res;
-		res.v.resize(v.size()+b.v.size(), 0);
-		for(unsigned i = 0; i < v.size(); ++i)
+		res.len = len + b.len;
+		for(int i = 0; i < len; ++i)
 		{
-			for(unsigned j = 0; j < b.v.size(); ++j)
+			for(int j = 0; j < b.len; ++j)
 			{
 				res.v[i+j] += v[i] * b.v[j];
 				res.v[i+j+1] += res.v[i+j] / BASE;
 				res.v[i+j] %= BASE;
 			}
 		}
-		if(res.v.back() == 0) res.v.pop_back();
+		if(res.v[res.len-1] == 0) res.len--;
 		return res;
 	}
 	BigInteger operator *= (const BigInteger &b) { *this = *this * b; return *this; }
@@ -82,8 +80,8 @@ BigInteger ans, p, maxf, f[Maxn][Maxn];
 template <typename T> inline T max(T &a, T &b) { return a < b ? b : a; }
 ostream& operator << (ostream &os, BigInteger &big)
 {
-	os << big.v.back();
-	for(int i = big.v.size()-2; i >= 0; --i)
+	os << big.v[big.len-1];
+	for(int i = big.len-2; i >= 0; --i)
 	{
 		char buf[5];
 		sprintf(buf, "%04d", big.v[i]);
@@ -109,18 +107,13 @@ int main()
 				r = k - l;
 				if(l - 1 >= 0) f[l][r] = max(f[l][r], f[l-1][r]+p*a[l]);
 				if(r - 1 >= 0) f[l][r] = max(f[l][r], f[l][r-1]+p*a[m-r+1]);
-#ifdef DEBUG
-				cout << l << " " << r << " " << f[l][r] << endl;
-#endif
 			}
 		}
 		maxf = 0;
 		for(int l = 0; l <= m; ++l) maxf = max(maxf, f[l][m-l]);
 		ans += maxf;
-#ifdef DEBUG
-		cout << ans << endl;
-#endif
 	}
 	cout << ans << endl;
 	return 0;
 }
+
