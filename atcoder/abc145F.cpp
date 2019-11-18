5922a4126a22c7dbc4c88b2b2d0ed8cd62cad2ac
Kaizyn <2291443901@qq.com>
2019-11-18 20:28:10 +0800

    backup

--- atcoder/abc145F.cpp
+++ atcoder/abc145F.cpp
@@ -1,23 +1,38 @@
 #include <bits/stdc++.h>
+// #define DEBUG
 
 using namespace std;
 
 const int N = 3e2+7;
+const long long INF = 1e18;
 
 int n, k;
 int h[N];
-// dp[i][j] at i'th delte j's(include i'th) <times, last height>
-pair<long long, int> dp[N][N];
+long long res = INF;
+// dp[i][j] keep i'th , del j's before i
+long long dp[N][N];
 
 int main()
 {
     cin >> n >> k;
-    h[0] = 0;
-    for (int i = 1; i <= n; ++i) {
-        cin >> h[i];
-        for (int j = 0; j <= min(j, k); ++j) {
-            dp[i][j] = dp[i-1][j];
+    for (int i = 1; i <= n; ++i) cin >> h[i];
+
+    for (int i = 0; i <= n+1; ++i)
+        for (int j = 0; j <= n+1; ++j)
+            dp[i][j] = INF;
+    dp[0][0] = 0;
+
+    for (int i = 1; i <= n+1; ++i) {
+        for (int j = 0; j < i && j <= k; ++j) {
+            for (int k = 0; k <= j && i-k-1 >= 0; ++k) {
+                dp[i][j] = min(dp[i][j], dp[i-k-1][j-k]+max(0, h[i]-h[i-k-1]));
+            }
+#ifdef DEBUG
+            printf("dp[%d][%d]=%lld\n", i, j, dp[i][j]);
+#endif
+            if (i == n+1) res = min(res, dp[i][j]);
         }
     }
+    cout << res << endl;
     return 0;
 }
