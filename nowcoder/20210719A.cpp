26e27a090c55304a256d18a35b331ed4386a1fce
Kaizyn <2291443901@qq.com>
2021-07-19 23:45:49 +0800

    emm,A

---
+++ nowcoder/20210719A.cpp
@@ -0,0 +1,135 @@
+/*
+ * @Author: Kaizyn
+ * @Date: 2021-07-19 22:58:04
+ * @LastEditTime: 2021-07-19 23:42:36
+ */
+#include <bits/stdc++.h>
+
+// #define DEBUG
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+const double eps = 1e-7;
+const double PI = acos(-1);
+const int MOD = 998244353; // 1e9+7;
+const int INF = 0x3f3f3f3f;
+// const ll INF = 1e18;
+const int N = 1e5+7;
+
+template <typename T, typename U = greater<T>>
+struct SegmentTree {
+  U cmp = U();
+  int n;
+  T tr[N<<2], lazy[N<<2], init_val = cmp(0, 1) ? INF : -INF;
+  SegmentTree(){}
+  T mv(const T &x, const T &y) { return cmp(x, y) ? x : y;}
+  void build(const int &_n, const T &k = 0) { n = _n; _build(1, n, k); }
+  template <typename TT>
+  void build(const TT a[], const int &_n) { n = _n; _build(a, 1, n); }
+  void modify(const int &x, const T &k) { _modify(x, k, 1, n); }
+  void add(const int &x, const T &k) { _add(x, x, k, 1, n); }
+  void add(const int &l, const int &r, const T &k) { _add(l, r, k, 1, n); }
+  T query(const int &x) { return _query(x, x, 1, n); }
+  T query(const int &l, const int &r) { return _query(l, r, 1, n); }
+private :
+  void push_up(const int &i) { tr[i] = mv(tr[i<<1], tr[i<<1|1]); }
+  void push_down(const int &i) {
+    if (!lazy[i]) return;
+    tr[i<<1] += lazy[i];
+    tr[i<<1|1] += lazy[i];
+    lazy[i<<1] += lazy[i];
+    lazy[i<<1|1] += lazy[i];
+    lazy[i] = 0;
+  }
+  void _build(const int &l, const int &r, const T &k = 0, const int &i = 1) {
+    lazy[i] = 0;
+    if (l == r) { tr[i] = k; return; }
+    int mid = (l+r)>>1;
+    _build(l, mid, k, i<<1);
+    _build(mid+1, r, k, i<<1|1);
+    push_up(i);
+  }
+  template <typename TT>
+  void _build(const TT a[], const int &l, const int &r, const int &i = 1) {
+    lazy[i] = 0;
+    if (l == r) { tr[i] = a[l]; return; }
+    int mid = (l+r)>>1;
+    _build(a, l, mid, i<<1);
+    _build(a, mid+1, r, i<<1|1);
+    push_up(i);
+  }
+  void _modify(const int &x, const T &k, const int &trl, const int &trr, const int &i = 1) {
+    if (trl == x && trr == x) return tr[i] = k, void();
+    push_down(i);
+    int mid = (trl+trr)>>1;
+    if (x <= mid) _modify(x, k, trl, mid, i<<1);
+    else _modify(x, k, mid+1, trr, i<<1|1);
+    push_up(i);
+  }
+  void _add(const int &l, const int &r, const T &k, const int &trl, const int &trr, const int &i = 1) {
+    if (trl >= l && trr <= r) {
+      tr[i] += k;
+      lazy[i] += k;
+      return;
+    }
+    push_down(i);
+    int mid = (trl+trr)>>1;
+    if (l <= mid) _add(l, r, k, trl, mid, i<<1);
+    if (r >  mid) _add(l, r, k, mid+1, trr, i<<1|1);
+    push_up(i);
+  }
+  T _query(const int &l, const int &r, const int &trl, const int &trr, const int &i = 1) {
+    if (trl >= l && trr <= r) return tr[i];
+    push_down(i);
+    int mid = (trl+trr)>>1;
+    if (r <=  mid) return _query(l, r, trl, mid, i<<1);
+    if (l >  mid) return _query(l, r, mid+1, trr, i<<1|1);
+    return mv(_query(l, r, trl, mid, i<<1), _query(l, r, mid+1, trr, i<<1|1));
+  }
+};
+
+int n;
+// g[i] the gcd(b[i, r])
+int a[N], b[N], g[N];
+SegmentTree<int> tree;
+
+inline void solve() {
+  cin >> n;
+  for (int i = 1; i <= n; ++i) cin >> a[i];
+  for (int i = 1; i < n; ++i) b[i] = a[i+1]-a[i];
+  tree.build(n);
+  ll res = 0;
+  for (int r = 1; r <= n; ++r) {
+    g[r] = a[r];
+    // 对于log个不同gcd值区间分别区间加gcd
+    // 单调栈维护?
+    // 对于gcd改变的点暴力单点修改
+    // 每个点至多改变log次,总复杂度nlog^2
+    for (int l = r-1; l; --l) {
+      if (__gcd(g[l], b[r]) != g[l]) {
+        tree.modify(l, );
+      } else {
+        break;
+      }
+    }
+    // 单调栈max min
+
+    // 询问值为0的个数
+    res += tree.query();
+  }
+  cout << res << '\n';
+}
+
+signed main() {
+#ifdef ONLINE_JUDGE
+  ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#endif
+  int T = 1;
+  cin >> T; // scanf("%d", &T);
+  for (int t = 1; t <= T; ++t) {
+    solve();
+  }
+  return 0;
+}
